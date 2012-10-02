3de2e5c403da01adbbb3d20da23ce8d60c048bc5
Emiko TAMIYA <emiko.tamiya@gmail.com>
2012-10-02 17:16:11 +0900

    modified

---
+++ app/models/ability.rb
@@ -0,0 +1,40 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user, ip_address = nil)
+    case user.try(:role).try(:name)
+    when 'Administrator'
+      can :manage, [
+        Basket,
+        CheckedItem,
+        Checkin,
+        Checkout,
+        LendingPolicy,
+        Reserve
+      ]
+    when 'Librarian'
+      can [:index, :create, :update, :destroy, :show], Reserve
+      can :manage, [
+        Basket,
+        CheckedItem,
+        Checkin,
+        Checkout,
+        Reserve
+      ]
+      can :read, [
+        LendingPolicy,
+      ]
+    when 'User'
+      can [:index, :create], Checkout
+      can [:show, :update, :destroy], Checkout do |checkout|
+        checkout.user == user
+      end
+      can [:index, :create], Reserve
+      can [:show, :update, :destroy], Reserve do |reserve|
+        reserve.user == user && reserve.expired_at.end_of_day > Time.zone.now
+      end
+    else
+      can :index, Checkout
+    end
+  end
+end
---
+++ app/models/manifestation.rb
@@ -0,0 +1,95 @@
+class Manifestation < ActiveRecord::Base
+  def next_reservation
+    self.reserves.not_retained.order('reserves.position ASC').first
+  end
+
+  def next_reserve
+    self.reserves.not_retained.order('reserves.position ASC').first
+  end
+
+  def is_reserved_by(user = nil)
+    if user
+      Reserve.waiting.where(:user_id => user.id, :manifestation_id => self.id).first
+    else
+      false
+    end
+  end
+
+  def is_reserved?
+    if self.reserves.present?
+      true
+    else
+      false
+    end
+  end
+end
+
+# == Schema Information
+#
+# Table name: manifestations
+#
+#  id                              :integer         not null, primary key
+#  original_title                  :text            not null
+#  title_alternative               :text
+#  title_transcription             :text
+#  classification_number           :string(255)
+#  identifier                      :string(255)
+#  date_of_publication             :datetime
+#  date_copyrighted                :datetime
+#  created_at                      :datetime
+#  updated_at                      :datetime
+#  deleted_at                      :datetime
+#  access_address                  :string(255)
+#  language_id                     :integer         default(1), not null
+#  carrier_type_id                 :integer         default(1), not null
+#  extent_id                       :integer         default(1), not null
+#  start_page                      :integer
+#  end_page                        :integer
+#  height                          :decimal(, )
+#  width                           :decimal(, )
+#  depth                           :decimal(, )
+#  isbn                            :string(255)
+#  isbn10                          :string(255)
+#  wrong_isbn                      :string(255)
+#  nbn                             :string(255)
+#  lccn                            :string(255)
+#  oclc_number                     :string(255)
+#  issn                            :string(255)
+#  price                           :integer
+#  fulltext                        :text
+#  volume_number_string              :string(255)
+#  issue_number_string               :string(255)
+#  serial_number_string              :string(255)
+#  edition                         :integer
+#  note                            :text
+#  produces_count                  :integer         default(0), not null
+#  exemplifies_count               :integer         default(0), not null
+#  embodies_count                  :integer         default(0), not null
+#  work_has_subjects_count         :integer         default(0), not null
+#  repository_content              :boolean         default(FALSE), not null
+#  lock_version                    :integer         default(0), not null
+#  required_role_id                :integer         default(1), not null
+#  state                           :string(255)
+#  required_score                  :integer         default(0), not null
+#  frequency_id                    :integer         default(1), not null
+#  subscription_master             :boolean         default(FALSE), not null
+#  ipaper_id                       :integer
+#  ipaper_access_key               :string(255)
+#  attachment_file_name            :string(255)
+#  attachment_content_type         :string(255)
+#  attachment_file_size            :integer
+#  attachment_updated_at           :datetime
+#  nii_type_id                     :integer
+#  title_alternative_transcription :text
+#  description                     :text
+#  abstract                        :text
+#  available_at                    :datetime
+#  valid_until                     :datetime
+#  date_submitted                  :datetime
+#  date_accepted                   :datetime
+#  date_caputured                  :datetime
+#  file_hash                       :string(255)
+#  pub_date                        :string(255)
+#  periodical_master               :boolean         default(FALSE), not null
+#
+
