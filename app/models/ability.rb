5453f3b68845e04c675ec29c070774b1b65c95ed
Emiko TAMIYA <emiko.tamiya@gmail.com>
2012-10-04 14:37:46 +0900

    modified initialize method name

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,5 @@
 class Ability
-  include CanCan::Ability
-
-  def initialize(user, ip_address = nil)
+  def initialize_circulation(user, ip_address = nil)
     case user.try(:role).try(:name)
     when 'Administrator'
       can :manage, [
