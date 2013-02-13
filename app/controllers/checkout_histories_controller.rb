2b44117ce8b048f79419850a91061c8d4f3398ec
Emiko TAMIYA <emiko.tamiya@gmail.com>
2013-02-13 19:40:31 +0900

    access denied without log in

--- app/controllers/checkout_histories_controller.rb
+++ app/controllers/checkout_histories_controller.rb
@@ -1,6 +1,7 @@
 class CheckoutHistoriesController < ApplicationController
   add_breadcrumb "I18n.t('activerecord.models.checkout_history')", 'checkout_histories_path'
   def index
+    access_denied unless current_user
     if current_user.has_role?('Librarian')
       @checkout_histories = CheckoutHistory.where(["DATE(created_at) = DATE(?)", Time.now]).page params[:page]
     else
