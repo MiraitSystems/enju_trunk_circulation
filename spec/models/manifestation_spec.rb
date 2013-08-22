4fc889160c9d5fcb1ac2f5d71d7d45a206cc25aa
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-22 17:46:11 +0900

    refs5611 add manifestation _spec file

---
+++ spec/models/manifestation_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe Manifestation, :solr => true do
+  describe '#next_reservation' do
+    #TODO
+  end
+
+  describe '#next_reserve' do
+    #TODO
+  end
+
+  describe '#is_reserved_by' do
+    #TODO
+  end
+
+  describe '#is_reserved?' do
+    #TODO
+  end
+end
