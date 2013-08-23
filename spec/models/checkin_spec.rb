adcc8a08c74306ef1b130f47e7bee114cf221b2a
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-23 15:07:29 +0900

    refs5616 modified spec file of checkin

--- spec/models/checkin_spec.rb
+++ spec/models/checkin_spec.rb
@@ -3,7 +3,25 @@ require 'spec_helper'
 
 describe Checkin do
   #pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :all
 
+  describe '#create' do
+    #validates_presence_of :item_identifier, :on => :create
+    it '' # TODO
+  end
+
+  describe '#update' do
+    #validates_presence_of :item, :basket, :on => :update
+    #validates_associated :item, :librarian, :basket, :on => :update
+  end
+
+  describe '#item_checkin' do
+    it '' # TODO: immediately
+  end
+
+  describe '#store_history' do
+    it '' # TODO
+  end
 end
 
 # == Schema Information
