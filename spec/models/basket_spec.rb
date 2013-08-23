793af8e8e004fa31941cbe6285e2392be6785b40
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-23 14:28:55 +0900

    refs5671 add test of basket model

--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -5,8 +5,40 @@ describe Basket do
   #pending "add some examples to (or delete) #{__FILE__}"
   fixtures :all
 
-  it "should not create basket when user is not active" do
-    Basket.create(:user => users(:user4)).id.should be_nil
+  describe '' do
+    describe '' do
+      #validates_associated :user, :on => :create
+      it '' #TODO:
+    end 
+    describe '' do
+      # 貸出完了後にかごのユーザidは破棄する
+      #validates_presence_of :user, :on => :create
+    end
+    describe '' do
+      # validate :check_suspended
+    end
+  end
+
+  describe '#create' do
+    it "should not create basket when user is not active" do
+      Basket.create(:user => users(:user4)).id.should be_nil
+    end
+  end
+
+  describe '#check_suspended' do
+    it '' #TODO: immediately
+  end
+
+  describe '#basket_checkout' do
+    it '' #TODO: immediately
+  end
+
+  describe '.expire' do
+    it '' #TODO
+  end
+
+  describe '#update_checked_items' do
+    it '' #TODO
   end
 end
 
