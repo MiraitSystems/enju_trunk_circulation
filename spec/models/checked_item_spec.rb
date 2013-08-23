1692a7e724c42e0633c03b9ce823318c9a75937b
Reiko Shinozuka <shinozuka.reiko@miraitsystems.jp>
2013-08-23 17:00:30 +0900

    refs5672 modieid spec file of checked_item

--- spec/models/checked_item_spec.rb
+++ spec/models/checked_item_spec.rb
@@ -4,8 +4,56 @@ require 'spec_helper'
 describe CheckedItem do
   fixtures :all
 
-  it "should respond to available_for_checkout?" do
-    checked_items(:checked_item_00001).available_for_checkout?.should_not be_true
+  describe 'validates' do
+    describe '' do
+      #validates_associated :item, :basket, :on => :update
+      it '' # TODO
+    end
+    describe '' do
+      #validates_presence_of :item, :basket, :due_date, :on => :update
+      it '' # TODO
+    end
+    describe '' do
+      #validates_uniqueness_of :item_id, :scope => :basket_id
+    end
+    describe '' do
+      #validate :available_for_checkout?, :on => :create
+    end
+    describe '' do
+      #before_validation :set_due_date, :on => :create
+    end
+  end
+ 
+  describe '#available_for_checkout?' do
+    it '' # TODO: immediately
+    # 以下のテストじゃ足らないので強化すること
+    it "should respond to available_for_checkout?" do
+      checked_items(:checked_item_00001).available_for_checkout?.should_not be_true
+    end
+  end
+ 
+  describe '#item_checkout_type' do
+    it '' # TODO
+  end
+ 
+  describe '#set_due_date' do
+    it '' # TODO: immediately
+  end
+ 
+  describe '#in_transaction?' do
+    it '' # TODO
+  end
+ 
+  describe '#destroy_reservation' do
+    it '' # TODO: immediately
+  end
+ 
+  describe '#available_for_reserve_checkout?' do
+    it '' # TODO: immediately
+  end
+ 
+  describe '#exchange_reserve_item' do
+    it '' # TODO: immediately
   end
 end
 
