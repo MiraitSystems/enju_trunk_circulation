4cf7dc856e0a7db2b54ba166484e8e6dc9a66cab
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2013-09-20 22:04:43 +0900

    新規作成

---
+++ spec/controllers/baskets_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe BasketsController do
+  fixtures :all
+
+  describe "GET new" do
+    describe "When logged in as Administrator" do
+
+    end
+
+    describe "When logged in as Librarian" do
+
+    end
+
+    describe "When logged in as User" do
+
+    end
+
+    describe "When not logged in" do
+
+    end
+  end
+end
+
